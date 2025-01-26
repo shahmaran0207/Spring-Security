55ba292a1fb3fa84bfc42830627f44324eacc826
shahmaran0207 <shahmaran0207@naver.com>
2025-01-26 14:35:59 +0900

    start new project

---
+++ src/main/java/com/Spring/Security/Controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.Spring.Security.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+}
